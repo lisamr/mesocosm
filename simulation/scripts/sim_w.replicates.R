e2a0d7964ad31374d037789b2a2b96fa7ac7ffd5
Lisa Rosenthal <lrosenthal@ucdavis.edu>
2018-12-05 09:54:19 -0800

    got results function working. just need to supply abundance dataframe, simulation function and need to manually change n.reps inside `results` if needed. don't know why I can't make n.reps an argument in the function, but oh well.

--- simulation/scripts/sim_w.replicates.R
+++ simulation/scripts/sim_w.replicates.R
@@ -9,6 +9,7 @@ A <- getabund2(n=100, mu=1)
 
 #run simulation
 s1 <- function(a, x){
+  #a=abundance dataframe; x=col in the dataframe
   t <- 15
   h <- format.comp(CV, a[,x], t)
   HP <- HPraster(h, 10, 10, 10)
@@ -73,10 +74,13 @@ bres.sum <- plyr::ldply(bres.sum, data.frame)
 ############################################################
 #An attempt to generalize
 ############################################################
+#competency values
+CV <- read.csv("simulation/outputs/competencyvalues.csv")
 A <- getabund2(n=100, mu=1)
 
 #simulation function ("f.sim" below)
 s1 <- function(a, x){
+  #a=abundance dataframe; x=col in the dataframe
   t <- 15
   h <- format.comp(CV, a[,x], t)
   HP <- HPraster(h, 10, 10, 10)
@@ -91,45 +95,76 @@ s1 <- function(a, x){
   return(list("format.comp"=h, "HPraster"=HP, "simulate"=sim, "response"=RV1, "respons.sum"=RV2))
 }
 
-#functions to bind results
-bind.response <- function(r){
-  #response
-  tmp1 <- lapply(1:4, function(x) do.call("rbind", r[[x]][4]))
-  tmp2 <- lapply(1:4, function(x) cbind(ident[x,], tmp1[[x]]))
-  plyr::ldply(tmp2, data.frame)
-}
-bind.respsum <- function(r){
-  #response summary
-  tmp4 <- lapply(1:4, function(x) do.call("rbind", r[[x]][5]))
-  tmp5 <- lapply(1:4, function(x) cbind(ident[x,], tmp4[[x]]))
-  plyr::ldply(tmp5, data.frame)
-}
-
-results <- function(A, n.reps, f.sim){
-
-  #richness levels for each community
+results <- function(A, f.sim){
+  nreps=10
   R <- apply(A, 2, function(x) length(unique(x)))
+  
+  #add binding functions
+  bind.response <- function(r){
+    #response
+    tmp1 <- lapply(1:4, function(x) do.call("rbind", r[[x]][4]))
+    tmp2 <- lapply(1:4, function(x) cbind(ident[x,], tmp1[[x]]))
+    plyr::ldply(tmp2, data.frame)
+  }
+  bind.respsum <- function(r){
+    #response summary
+    tmp4 <- lapply(1:4, function(x) do.call("rbind", r[[x]][5]))
+    tmp5 <- lapply(1:4, function(x) cbind(ident[x,], tmp4[[x]]))
+    plyr::ldply(tmp5, data.frame)
+  }
   
   #1. response
   bres <- list(NULL)
-  ident <- data.frame(dens="sub", assemb="det", rich=R, rep=i)
-  for(i in 1:nreps){
-    res1 <- lapply(1:4, function(x) f.sim(A, x))
-    bres[[i]] <- bind.response(res1)
-  }
-  bres <- plyr::ldply(bres, data.frame)
-  
-  #2. response summary
-  bres.sum <- list(NULL)
   for(i in 1:nreps){
     res1 <- lapply(1:4, function(x) f.sim(A, x))
     ident <- data.frame(rep=i, dens="sub", assemb="det", rich=R)
-    bres.sum[[i]] <- bind.respsum(res1)
+    bres[[i]] <- bind.response(res1)
   }
-  bres.sum <- plyr::ldply(bres.sum, data.frame)
+  bres <- plyr::ldply(bres, data.frame)
+  #2. response summary
+  bres2 <- list(NULL)
+  for(i in 1:nreps){
+    res1 <- lapply(1:4, function(x) f.sim(A, x))
+    ident <- data.frame(rep=i, dens="sub", assemb="det", rich=R)
+    bres2[[i]] <- bind.respsum(res1)
+  }
+  bres2 <- plyr::ldply(bres2, data.frame)
   
-  list("responses"=bres, "response.sum"=bres.sum)
+  return(list("responses"= bres, "response.summary"= bres2))
 }
 
-results1 <- results(A = A, nreps=5, f.sim = s1)
-results1$responses %>% summary()
+#get results
+R1 <- results(A, s1)
+Rt1 <- R1$responses
+Rsum1 <- R1$response.summary
+
+#summary plots
+head(Rsum1)
+Rsum2 <- Rsum1 %>% 
+  filter(species=="tot")
+head(Rsum2)
+
+ggplot(Rsum2, aes(rich, pI, group=rep)) +
+  geom_point()+
+  geom_line()
+
+ggplot(Rsum2, aes(rich, max.dI, group=rep)) +
+  geom_point()+
+  geom_line()
+
+#responses over time
+head(Rt1)
+ggplot(filter(Rt1, species=="tot"), aes(time, pI, group=rep)) +
+  geom_point()+
+  geom_line() +
+  facet_wrap(~rich)
+
+ggplot(filter(Rt1, species=="tot"), aes(time, dI, group=rep)) +
+  geom_point()+
+  geom_line() +
+  facet_wrap(~rich)
+
+ggplot(filter(Rt1, species=="tot"), aes(time, n.E, group=rep)) +
+  geom_point()+
+  geom_line() +
+  facet_wrap(~rich)
